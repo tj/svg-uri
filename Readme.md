2a8131e51940c004537bfcae98fcf72bbc565b6a
TJ Holowaychuk <tj@vision-media.ca>
2017-02-24 09:24:37 -0800

    Initial commit

---
+++ Readme.md
@@ -0,0 +1,23 @@
+
+# svg-uri
+
+Output a data uri, transparently compressing with svgo when installed. For those times when you only need a few small SVGS and want to inline them in your CSS.
+
+## Installation
+
+```
+$ go get github.com/tj/svg-uri
+```
+
+## Badges
+
+[![GoDoc](https://godoc.org/github.com/tj/svg-uri?status.svg)](https://godoc.org/github.com/tj/svg-uri)
+![](https://img.shields.io/badge/license-MIT-blue.svg)
+![](https://img.shields.io/badge/status-stable-green.svg)
+[![](http://apex.sh/images/badge.svg)](https://apex.sh/ping/)
+
+---
+
+> [tjholowaychuk.com](http://tjholowaychuk.com) &nbsp;&middot;&nbsp;
+> GitHub [@tj](https://github.com/tj) &nbsp;&middot;&nbsp;
+> Twitter [@tjholowaychuk](https://twitter.com/tjholowaychuk)
---
+++ main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"bytes"
+	"encoding/base64"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"os/exec"
+)
+
+var usage = `
+  Usage: svg-uri
+
+  Examples:
+
+    svg-uri < my.svg
+`
+
+func main() {
+	flag.Usage = func() {
+		fmt.Printf("%s\n", usage)
+	}
+
+	flag.Parse()
+
+	svgo, _ := exec.LookPath("svgo")
+
+	// input
+	b, err := ioutil.ReadAll(os.Stdin)
+	if err != nil {
+		log.Fatalf("error reading: %s", err)
+	}
+
+	// compress
+	if svgo != "" {
+		var out bytes.Buffer
+		cmd := exec.Command(svgo, "-i", "-")
+		cmd.Stdin = bytes.NewReader(b)
+		cmd.Stdout = &out
+		err := cmd.Run()
+		if err != nil {
+			log.Fatalf("error passing through svgo: %s", err)
+		}
+		b = out.Bytes()
+	}
+
+	// output
+	s := base64.URLEncoding.EncodeToString(b)
+	fmt.Printf(`url("data:image/svg+xml;base64,%s")`, s)
+}
